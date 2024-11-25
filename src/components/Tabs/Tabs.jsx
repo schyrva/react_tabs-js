97cd53fbf1485ef180472d2324f7446f68a5dc33
Stanislav Chyrva <alkefirko@gmail.com>
2024-11-25 20:35:45 +0200

    fixed lint problems

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,5 +1,5 @@
 import cn from 'classnames';
-/* eslint-disable jsx-a11y/click-events-have-key-events, jsx-a11y/no-noninteractive-element-interactions */
+
 export const Tabs = ({ tabs, activeTabId, onTabSelected }) => {
   const activeTab = tabs.find(tab => tab.id === activeTabId) || tabs[0];
   const handleTabClick = tabId => {
@@ -16,10 +16,13 @@ export const Tabs = ({ tabs, activeTabId, onTabSelected }) => {
             <li
               key={tab.id}
               className={cn({ 'is-active': tab.id === activeTab.id })}
-              onClick={() => handleTabClick(tab.id)}
               data-cy="Tab"
             >
-              <a href={`#${tab.id}`} data-cy="TabLink">
+              <a
+                onClick={() => handleTabClick(tab.id)}
+                href={`#${tab.id}`}
+                data-cy="TabLink"
+              >
                 {tab.title}
               </a>
             </li>
